4bb216e8b78c7724d1806523bdafa349d34b4b87
Joe Stump <jstump@php.net>
2008-05-03 19:41:24 +0000

    - Removed deprecated cURL option CURLOPT_MUTE (Bug #13489) - Changed the license to the New BSD License (Bug #13831) - Cleaned up code to (mostly) comply with phpcs
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_Curl/trunk@258990 c90b9560-bf6c-de11-be94-00142212c4b1

--- Net/Curl.php
+++ Net/Curl.php
@@ -1,39 +1,66 @@
 <?php
 
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
-
 /**
- * Net_Curl
+ * An Object Oriented interface to PHP's cURL extension
  *
- * LICENSE: This source file is subject to version 3.0 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
+ * PHP version 5.1.0+
  *
- * @category   Net
- * @package    Net_Curl
- * @author     David Costa <gurugeek@php.net>
- * @author     Sterling Hughes <sterling@php.net>
- * @author     Joe Stump <joe@joestump.net>
- * @author     Philippe Jausions <jausions@php.net>
- * @copyright  1997-2005 The PHP Group
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version    CVS: $Revision$
- * @link       http://pear.php.net/package/Net_Curl
+ * Copyright (c) 2007, The PEAR Group
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *  - Neither the name of the The PEAR Group nor the names of its contributors 
+ *    may be used to endorse or promote products derived from this software 
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category  Net
+ * @package   Net_Curl
+ * @author    David Costa <gurugeek@php.net>
+ * @author    Sterling Hughes <sterling@php.net>
+ * @author    Joe Stump <joe@joestump.net>
+ * @author    Philippe Jausions <jausions@php.net>
+ * @copyright 1997-2008 The PHP Group
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   CVS: $Revision$
+ * @link      http://pear.php.net/package/Net_Curl
  */
 
 /**
  * Include PEAR package for error handling
  */
-require_once('PEAR.php');
+require_once 'PEAR.php';
 
 /**
  * Object-oriented implementation of the Curl extension
  *
- * @category   Net
- * @package    Net_Curl
- * @link       http://pear.php.net/package/Net_Curl
+ * @category Net
+ * @package  Net_Curl
+ * @author   David Costa <gurugeek@php.net>
+ * @author   Sterling Hughes <sterling@php.net>
+ * @author   Joe Stump <joe@joestump.net>
+ * @author   Philippe Jausions <jausions@php.net>
+ * @license  http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link     http://pear.php.net/package/Net_Curl
  */
 class Net_Curl
 {
@@ -346,10 +373,14 @@ class Net_Curl
      * The Net_Curl PHP 5.x constructor, called when a new Net_Curl object
      * is initialized (also called via 4.x constructor)
      *
-     * @param string $url The URL to fetch (can be set using the $url property as well)
-     * @param string $userAgent The userAgent string (can be set using the $userAgent property as well)
+     * @param string $url       The URL to fetch (can be set using the $url 
+     *                          property as well)
+     * @param string $userAgent The userAgent string (can be set using the 
+     *                          $userAgent property as well)
+     *
      * @access public
      * @author Joe Stump <joe@joestump.net>
+     * @return void
      */
     function __construct($url = '', $userAgent = '')
     {
@@ -369,8 +400,13 @@ class Net_Curl
      *
      * PHP 4.x constructor.
      *
+     * @param string $url       The URL to fetch (can be set using the $url 
+     *                          property as well)
+     * @param string $userAgent The userAgent string (can be set using the 
+     *                          $userAgent property as well)
+     *
      * @access public
-     * @author Joe Stump <joe@joestump.net>
+     * @return void
      */
     function Net_Curl($url = '', $userAgent = '')
     {
@@ -423,7 +459,7 @@ class Net_Curl
         $ret = true;
 
         // Basic stuff
-        $ret = curl_setopt($this->_ch, CURLOPT_URL,    $this->url);
+        $ret = curl_setopt($this->_ch, CURLOPT_URL, $this->url);
         $ret = curl_setopt($this->_ch, CURLOPT_HEADER, $this->header);
 
         // Whether or not to return the transfer contents
@@ -433,12 +469,16 @@ class Net_Curl
 
         // HTTP Authentication
         if ($this->username != '') {
-            $ret = curl_setopt($this->_ch, CURLOPT_USERPWD, $this->username . ':' . $this->password);
+            $ret = curl_setopt($this->_ch, 
+                               CURLOPT_USERPWD, 
+                               $this->username . ':' . $this->password);
         }
 
         // SSL Checks
         if (isset($this->sslVersion)) {
-            $ret = curl_setopt($this->_ch, CURLOPT_SSLVERSION, $this->sslVersion);
+            $ret = curl_setopt($this->_ch, 
+                               CURLOPT_SSLVERSION, 
+                               $this->sslVersion);
         }
 
         if (isset($this->sslCert)) {
@@ -446,7 +486,9 @@ class Net_Curl
         }
 
         if (isset($this->sslCertPasswd)) {
-            $ret = curl_setopt($this->_ch, CURLOPT_SSLCERTPASSWD, $this->sslCertPasswd);
+            $ret = curl_setopt($this->_ch, 
+                               CURLOPT_SSLCERTPASSWD, 
+                               $this->sslCertPasswd);
         }
 
         // Proxy Related checks
@@ -455,18 +497,20 @@ class Net_Curl
         }
 
         if (isset($this->proxyUser) || isset($this->proxyPassword)) {
-            $ret = curl_setopt($this->_ch, CURLOPT_PROXYUSERPWD, $this->proxyUser . ':' . $this->proxyPassword);
+            $ret = curl_setopt($this->_ch, 
+                               CURLOPT_PROXYUSERPWD, 
+                               $this->proxyUser . ':' . $this->proxyPassword);
         }
 
         if (is_bool($this->verifyPeer)) {
-            if(!$this->setOption(CURLOPT_SSL_VERIFYPEER,$this->verifyPeer)) {
+            if (!$this->setOption(CURLOPT_SSL_VERIFYPEER, $this->verifyPeer)) {
                 return PEAR::raiseError('Error setting CURLOPT_SSL_VERIFYPEER');
             }
         }
 
         if (is_numeric($this->verifyHost) && $this->verifyHost >= 0 &&
             $this->verifyHost <= 2) {
-            if(!$this->setOption(CURLOPT_SSL_VERIFYHOST,$this->verifyHost)) {
+            if (!$this->setOption(CURLOPT_SSL_VERIFYHOST, $this->verifyHost)) {
                 return PEAR::raiseError('Error setting CURLOPT_SSL_VERIFYPEER');
             }
         }
@@ -474,16 +518,17 @@ class Net_Curl
         if (is_bool($this->verifyPeer) && $this->verifyPeer == true) {
             if (isset($this->caInfo) && strlen($this->caInfo)) {
                 if (file_exists($this->caInfo)) {
-                    if (!$this->setOption(CURLOPT_CAINFO,$this->caInfo)) {
+                    if (!$this->setOption(CURLOPT_CAINFO, $this->caInfo)) {
                         return PEAR::raiseError('Error setting CURLOPT_CAINFO');
                     }
                 } else {
-                    return PEAR::raiseError('Could not find CA info: '.$this->caInfo);
+                    return PEAR::raiseError('Could not find CA info: '.
+                                            $this->caInfo);
                 }
             }
 
             if (isset($this->caPath) && is_string($this->caPath)) {
-                if (!$this->setOption(CURLOPT_CAPATH,$this->caPath)) {
+                if (!$this->setOption(CURLOPT_CAPATH, $this->caPath)) {
                     return PEAR::raiseError('Error setting CURLOPT_CAPATH');
                 }
             }
@@ -507,7 +552,7 @@ class Net_Curl
                 return PEAR::raiseError('File does not exist: '.$this->file);
             }
 
-            $this->_fp = fopen($this->file,'r');
+            $this->_fp = fopen($this->file, 'r');
             if (!is_resource($this->_fp)) {
                 return PEAR::raiseError('Could not open file: '.$this->file);
             }
@@ -525,7 +570,7 @@ class Net_Curl
             $sets = null;
             if (!isset($this->type)) {
                 $this->type = 'post';
-                $ret = curl_setopt($this->_ch, CURLOPT_POST, true);
+                $ret        = curl_setopt($this->_ch, CURLOPT_POST, true);
             }
 
             // If fields is an array then turn it into a string. Sometimes
@@ -565,7 +610,9 @@ class Net_Curl
         }
 
         // If a Location: header is passed then follow it
-        $ret = curl_setopt($this->_ch, CURLOPT_FOLLOWLOCATION, $this->followLocation);
+        $ret = curl_setopt($this->_ch, 
+                           CURLOPT_FOLLOWLOCATION, 
+                           $this->followLocation);
 
         // If a timeout is set and is greater then zero then set it
         if (is_numeric($this->timeout) && $this->timeout > 0) {
@@ -589,7 +636,9 @@ class Net_Curl
         // Other HTTP headers
         if ($this->httpHeaders !== null) {
             if (is_array($this->httpHeaders)) {
-                $ret = curl_setopt($this->_ch, CURLOPT_HTTPHEADER, $this->httpHeaders);
+                $ret = curl_setopt($this->_ch, 
+                                   CURLOPT_HTTPHEADER, 
+                                   $this->httpHeaders);
             } else {
                 return PEAR::raiseError('Net_Curl::$httpHeaders must be an array');
             }
@@ -611,9 +660,10 @@ class Net_Curl
         if (!isset($info['http_code'])) {
             return PEAR::raiseError('Unknown or invalid HTTP response');
         } else {
-            $type = substr($info['http_code'],0,1);
+            $type = substr($info['http_code'], 0, 1);
             if ($type != 2 && $type != 3) {
-                return PEAR::raiseError('Unexpected HTTP code: '.$info['http_code']);
+                return PEAR::raiseError('Unexpected HTTP code: ' .
+                                        $info['http_code']);
             }
         }
 
@@ -644,10 +694,11 @@ class Net_Curl
      * ?>
      * </code>
      *
+     * @param int   $option cURL constant (ie. CURLOPT_URL)
+     * @param mixed $value  The option's value
+     * 
      * @author Joe Stump <joe@joestump.net>
      * @access public
-     * @param int $option cURL constant (ie. CURLOPT_URL)
-     * @param mixed $value
      * @return boolean
      */
     function setOption($option, $value)
@@ -718,9 +769,9 @@ class Net_Curl
      */
     function verboseAll()
     {
-        $this->verbose = true;
-        $this->mute = false;
-        $this->header = true;
+        $this->verbose  = true;
+        $this->mute     = false;
+        $this->header   = true;
         $this->progress = true;
     }
     // }}}
@@ -731,13 +782,13 @@ class Net_Curl
      *
      * @access public
      * @author David Costa <gurugeek@php.net>
+     * @return void
      * @deprecated
      */
     function verbose_all()
     {
         $this->verboseAll();
-        PEAR::raiseError('Net_Curl::verbose_all() is deprecated! Please use Net_Curl::verboseAll()'." <br />\n",null,PEAR_ERROR_PRINT);
-
+        PEAR::raiseError('Net_Curl::verbose_all() is deprecated! Please use Net_Curl::verboseAll()'." <br />\n", null, PEAR_ERROR_PRINT);
     }
     // }}}
 
@@ -767,7 +818,8 @@ class Net_Curl
      * @access private
      * @return void
      */
-    function _mapDeprecatedVariables() {
+    function _mapDeprecatedVariables() 
+    {
         $bad = array();
         if ($this->follow_location !== false) {
             if ($this->follow_location > 0) {
@@ -791,16 +843,16 @@ class Net_Curl
 
         if ($this->file_size !== false) {
             $this->fileSize = $this->file_size;
-            $bad[] = array('file_size','fileSize');
+            $bad[]          = array('file_size', 'fileSize');
         }
 
         if ($this->http_headers !== false) {
             $this->httpHeaders = $this->http_headers;
-            $bad[] = array('http_headers','httpHeaders');
+            $bad[]             = array('http_headers', 'httpHeaders');
         }
 
         foreach ($bad as $map) {
-            PEAR::raiseError('Net_Curl::$'.$map[0].' is deprecated! Please use Net_Curl::$'.$map[1]." instead! <br />\n", null, PEAR_ERROR_PRINT);
+            PEAR::raiseError('Net_Curl::$'. $map[0]. ' is deprecated! Please use Net_Curl::$'.$map[1]." instead! <br />\n", null, PEAR_ERROR_PRINT);
         }
     }
     // }}}
